2aa50ac7a37d0421c5b74dcd5f3fa18f177c3747
laurovicimihaela <mihaela_laurovici@yahoo.com>
2022-05-11 21:01:57 +0300

    blogger main page added

---
+++ mern-recipes-factory/src/components/Main/index_blogger.jsx
@@ -0,0 +1,53 @@
+import styles from "./styles.module.css";
+import Dropdown from "./Dropdown";
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import CarouselContainer from "./CarouselContainer";
+
+const Main = () => {
+	const handleLogout = () => {
+		localStorage.removeItem("token");
+		window.location.reload();
+	};
+	const [selected, setSelected] = useState("Categorii");
+	const [selected1, setSelected1] = useState("Contul meu");
+	const options = ["Mic Dejun", "Fel Principal", "Supă", "Desert"];
+	const options1 = ["Salvate", "Profilul meu", "Rețetele mele", "Adaugă rețetă"];
+	return (
+		
+		<body>
+			<nav className={styles.navbar}>
+				<h1>The Recipes Factory</h1>
+				<div className={styles.allign2}>
+				<Dropdown selected={selected} setSelected={setSelected} options={options}/>
+				<br /> 
+				<br />
+				<br />
+				<br />
+				</div>
+
+				<div className={styles.allign1}>
+				<Dropdown selected={selected1} setSelected={setSelected1} options={options1}/>
+				<br />
+				<br />
+                <br />
+                <br />
+				</div>
+
+				<button className={styles.white_btn} onClick={handleLogout}>
+					Log out
+				</button>
+
+			</nav>
+		<div className={styles.container}>
+			
+			
+				<CarouselContainer/>
+			
+		</div>
+		
+		</body>
+	);
+};
+
+export default Main;
